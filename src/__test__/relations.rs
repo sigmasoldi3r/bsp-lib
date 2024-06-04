276b1d989640079b5c5095071464956a10dca173
Pablo Blanco Celdrán <pablobc.1995@gmail.com>
2024-06-04 09:15:35 +0200

    fix: n_edges was not what docs said

--- src/__test__/relations.rs
+++ src/__test__/relations.rs
@@ -1,19 +1,19 @@
 use std::fs::File;
 
-use crate::{bsp::Bsp, lumps::vertices};
-
+use crate::bsp::Bsp;
 
 const TEST_MAP: &'static str = "maps/crossfire.bsp";
 
 #[test]
 fn test_face_relation_functions() {
-  let mut file = File::open(TEST_MAP).unwrap();
-  let bsp = Bsp::parse(&mut file).unwrap();
-  for face in &bsp.faces.0 {
-    if let Some(edges) = face.edges(&bsp) {
-      for edge in edges {
-        let _ = edge.vertices(&bsp);
-      }
+    let mut file = File::open(TEST_MAP).unwrap();
+    let bsp = Bsp::parse(&mut file).unwrap();
+    let mut vertices_total = vec![];
+    for face in &bsp.faces.0 {
+        let vertices = face.vertices(&bsp);
+        for vertex in vertices {
+            vertices_total.push(vertex);
+        }
     }
-  }
+    println!("n = {}", vertices_total.len());
 }
--- src/relational.rs
+++ src/relational.rs
@@ -3,18 +3,11 @@ use crate::{
     lumps::{faces::BspFace, vertices::BspVertex},
 };
 
-// fn range_from<A, B>(from: &A, to: &B) -> Option<Range<usize>> {
-//   let from: usize = from.try_into()?;
-//   let to: usize = to.try_into()?;
-//   Some(from..to)
-// }
-
 impl BspFace {
     pub fn vertices<'a>(&self, bsp: &'a Bsp) -> Vec<&'a BspVertex> {
         let i_first_edge = self.i_first_edge as usize;
         let n_edges = (self.i_first_edge + self.n_edges as u32) as usize;
-        let i_last_edge = i_first_edge + n_edges;
-        let i_surf_edges = &bsp.surf_edges.0[i_first_edge..i_last_edge];
+        let i_surf_edges = &bsp.surf_edges.0[i_first_edge..n_edges];
         let mut vertices: Vec<&'a BspVertex> = vec![];
         for i_surf_edge in i_surf_edges {
             let index = i_surf_edge.0;
